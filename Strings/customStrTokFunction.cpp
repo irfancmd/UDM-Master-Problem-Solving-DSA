a14e5dfe491944a6d6f63a12b31770207700db67
Chowdhury Mohammad Irfanuddin <mohammad.irfanuddin@technonext.com>
2024-03-04 14:44:24 +0600

    custom string tokenizer function

---
+++ Strings/customStrTokFunction.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<cstring> // For len
+
+#include "../utils.hpp"
+
+char* customStrTok(char* input, char delim) {
+    static char* str {nullptr};
+
+    if(input != nullptr) {
+        str = input;
+    }
+
+    if(str == nullptr) {
+        return nullptr;
+    }
+
+    char *token = new char[strlen(str) + 1]; // +1 for null character
+    int i {0};
+
+    for(; input[i] != '\0'; i++) {
+        if(input[i] != delim) {
+            token[i] = input[i];
+        } else {
+            token[i] = '\0';
+            input = input + i + 1; // Move the pointer
+
+            return token;
+        }
+    }
+
+    token[i] = '\0';
+    // Reset the input.
+    input = nullptr;
+
+    return token;
+}
+
+int main() {
+    char input[100] {"Hello,world,Earth!"};
+
+    char *token = customStrTok(input, ',');
+    cout << token << endl;
+
+    char *token2 = customStrTok(nullptr, ',');
+    cout << token2 << endl;
+
+    char *token3 = customStrTok(nullptr, ',');
+    cout << token3 << endl;
+}
