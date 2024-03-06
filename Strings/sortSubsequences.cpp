8c14f3f3a28e6f2297667690d6ff0478a1fd7c05
Chowdhury Mohammad Irfanuddin <mohammad.irfanuddin@technonext.com>
2024-03-06 14:28:28 +0600

    Completed upto sort string subsequences

---
+++ Strings/sortSubsequences.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+#include<algorithm>
+
+#include "../utils.hpp"
+
+void subSequence(string s, string output, vector<string> &v) {
+    // Base case
+    if(s.size() == 0) {
+        v.push_back(output);
+        return;
+    }
+
+    // Recursion
+    char ch = s[0];
+    string reducedInput = s.substr(1);
+
+    // Includes the current letter
+    subSequence(reducedInput, output + ch, v);
+
+    // Excludes the current letter
+    subSequence(reducedInput, output, v);
+}
+
+bool compareStrings(string s1, string s2) {
+    if(s1.length() == s2.length()) {
+        return s1 < s2;
+    }
+
+    return s1.length() < s2.length();
+}
+
+int main() {
+    string s;
+    cin >> s;
+
+    vector<string> v;
+
+    string result = "";
+
+    subSequence(s, result, v);
+
+    sort(v.begin(), v.end(), compareStrings);
+
+    for(auto s : v){
+        cout << s << ",";
+    }
+    cout << endl;
+
+    return 0;
+}
+
+
